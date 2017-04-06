9810c282a0668147ccca746bfcd2251f3fac411d
eugene-fedurkin <eugene.fedurkin@gmail.com>
2017-04-06 17:12:20 +0300

    implement site

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let button = document.getElementById('play');
+button.onclick = function() {
+  location.href = 'https://alex-borel.github.io/simpleGame/index.html'
+}
+document.querySelectorAll('li')[0].onclick = function() {
+  location.href = 'index.html'
+}
+document.querySelectorAll('li')[1].onclick = function() {
+  location.href = 'index.eng.html'
+}
