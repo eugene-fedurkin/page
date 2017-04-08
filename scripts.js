c4563367c40bfd16154e55a71afd96f3eea279c8
eugene-fedurkin <eugene.fedurkin@gmail.com>
2017-04-08 11:49:41 +0300

    fixed implemention

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ let button = document.getElementById('play');
 button.onclick = function() {
   location.href = 'https://alex-borel.github.io/simpleGame/index.html'
 }
-document.querySelectorAll('li')[0].onclick = function() {
+document.querySelectorAll('li')[1].onclick = function() {
   location.href = 'index.html'
 }
-document.querySelectorAll('li')[1].onclick = function() {
+document.querySelectorAll('li')[0].onclick = function() {
   location.href = 'index.eng.html'
 }
